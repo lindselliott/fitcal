a05896dc84a4939a0a36d3420b2cae91dabb648e
lindsayelliott <lindsayelliott4@gmail.com>
2017-10-11 19:43:44 -0400

    Writing to file is working, these files can be imported to ical

--- fitcal.py
+++ fitcal.py
@@ -1,17 +1,14 @@
+from datetime import datetime 
 import json 
 import pdb 
 import sys
 import csv 
 
-
-data = {} 
-headings = [] 
-row_count = 0 
-
-with open(sys.argv[1], 'rb') as fitbit_csv:
-	fitbit_reader = csv.reader(fitbit_csv, delimiter=',', quotechar='"')
-	
+def read_fitbit_csv(fitbit_reader):
+	data = {} 
+	headings = [] 
 	row_count = 0 
+
 	for row in fitbit_reader:
 		if row_count == 0:
 			row_count = row_count + 1 
@@ -22,20 +19,79 @@ with open(sys.argv[1], 'rb') as fitbit_csv:
 
 		if row_count == 1: 
 			for element in row: 
-
 				headings.append(element)
 		else: 
 			data[row[0]] = {}
 
 			element_count = 0
 			for element in row:
-				if headings[element_count] == 'Date':
-					element_count = element_count + 1 
-					continue 
-
 				data[row[0]][headings[element_count]] = element
 				element_count = element_count + 1  
 
 		row_count = row_count + 1 
 
-print data 
+	return data 
+
+def write_event_property(ics_file, date): 
+	ics_file.write("BEGIN:VEVENT\n")
+	
+	prop_date = datetime.strptime(date['Date'], "%Y-%m-%d")
+	date_str = prop_date.strftime('%Y%m%d')
+
+	summary = 'SUMMARY:Fitbit Activity for %s\n' % date['Date']
+	ics_file.write(summary)
+
+	uid = 'UID:Fitbit-Activity-%s\n' % date_str
+	ics_file.write(uid)
+
+	#dtstamp = 'DTSTAMP:%sT120000Z\n' % date['Date']
+	#ics_file.write(dtstamp)
+
+	dtstart = 'DTSTART;TZID=America/Toronto:%sT235800\n' % date_str
+	ics_file.write(dtstart)
+
+	dtend = 'DTEND;TZID=America/Toronto:%sT235900\n' % date_str
+	ics_file.write(dtend)
+
+	ics_file.write("DESCRIPTION:")
+	for element in date: 
+		if element == 'Date':
+			continue 
+
+		description = '\t%s: %s\\n\n' % (element, date[element])
+		ics_file.write(description)
+
+	ics_file.write("END:VEVENT\n")
+
+
+def write_calendar_property(ics_file, data):
+	ics_file.write("BEGIN:VCALENDAR\n")
+	ics_file.write("PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN\n")
+	ics_file.write("VERSION:2.0\n")
+
+	for date in data: 
+		write_event_property(ics_file, data[date])
+
+	ics_file.write("END:VCALENDAR\n")
+
+
+def write_ics_file(data):
+	file = open("testing.ics", "w")
+	write_calendar_property(file, data)
+	file.close()
+
+	
+#-------------------------
+def main():
+	data = {} 
+	
+	with open(sys.argv[1], 'rb') as fitbit_csv:
+		fitbit_reader = csv.reader(fitbit_csv, delimiter=',', quotechar='"')
+			
+		data = read_fitbit_csv(fitbit_reader)
+
+	write_ics_file(data)
+
+
+
+main()
